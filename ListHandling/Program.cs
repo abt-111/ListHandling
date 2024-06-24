d2a78ae1f5248b71d5835c78cc8446ca154a57fb
abt-111 <anthony.temmel@gmail.com>
2024-06-24 16:49:11 +0200

    Ajout du code des opérations sur les listes

--- ListHandling/Program.cs
+++ ListHandling/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<String> listOfString = new List<String>();
+
+        listOfString.Add("Bonjour");
+        listOfString.Add(" le");
+        listOfString.Add(" monde");
+        DisplayListOfString(listOfString);
+
+        listOfString.RemoveAt(0);
+        DisplayListOfString(listOfString);
+
+        listOfString.Remove(" monde");
+        DisplayListOfString(listOfString);
+
+        listOfString[0] = "Bonjour le monde";
+        DisplayListOfString(listOfString);
+    }
+
+    public static void DisplayListOfString(List<String> listOfString)
+    {
+        int index = 0;
+
+        foreach (string s in listOfString)
+        {
+            Console.WriteLine($"listOfString[{index}] : {s}");
+            index++;
+        }
+        Console.WriteLine();
+    }
+}
